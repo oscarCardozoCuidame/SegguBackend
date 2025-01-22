9196249a3ad55beb4dbfc3fa42ce1aa8ef0ff1cc
oscarCardozoCuidame <ocardozo@cuidame.tech>
2025-01-22 11:51:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const setupAssociations = require('./src/config/associations');
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));  // Aquí se asegura el límite
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Rutas principales
 app.use("/seggu", require("./src/routes"));
@@ -14,7 +15,6 @@ app.use("/seggu", require("./src/routes"));
 // Sincronizar modelos con la base de datos y levantar el servidor
 (async () => {
     try {
-
         await setupAssociations();
 
         // Verificar la conexión con la base de datos
@@ -27,7 +27,7 @@ app.use("/seggu", require("./src/routes"));
 
         // Iniciar el servidor
         app.listen(4000, () => {
-            console.log("Server is running at http://localhost:4000");
+            console.log("Server is running");
         });
     } catch (error) {
         console.error("Unable to connect to the database:", error);
