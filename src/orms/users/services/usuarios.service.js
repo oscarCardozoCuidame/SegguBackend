e9e4c9739da725385cebb96903c7d8a1792bbf9e
oscarCardozoCuidame <ocardozo@cuidame.tech>
2025-01-22 16:20:05 -0500

    Update usuarios.service.js

--- src/orms/users/services/usuarios.service.js
+++ src/orms/users/services/usuarios.service.js
@@ -3,6 +3,7 @@ const { buildImage, deleteImage } = require('../../../utils/imageHandler');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const path = require("path");
+const fs = require("fs");
 
 const UserService = {
   getAllUsers: async () => {
